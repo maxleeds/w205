34082456acec357b0b1835d58c57646f856d50c8
maxleeds <30944219+maxleeds@users.noreply.github.com>
2017-08-13 18:34:28 -0400

    Update staticimport.sh

--- max/staticimport.sh
+++ max/staticimport.sh
@@ -1,7 +1,9 @@
-# import static list of stations
+
 if [ ! -d "$staging1" ]; then
   mkdir staging1
 fi
+
+# import static list of stations
 
 cd staging1
 url="http://web.mta.info/developers/data/nyct/subway/Stations.csv"
@@ -10,3 +12,9 @@ tail -n +2 stations.csv > stationsfinal.csv
 
 hdfs dfs -mkdir /user/w205/mtastatic
 hdfs dfs -put stationsfinal.csv /user/w205/mtastatic
+
+# import static list of complexes
+url1="http://web.mta.info/developers/data/nyct/subway/Stations.csv"
+wget $url1 -O complexes.csv
+tail -n +2 complexes.csv > complexesfinal.csv
+hdfs dfs -put complexesfinal.csv /user/w205/mtastatic

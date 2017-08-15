d55da2ea528442992b3b50b952c63c49fc42dc37
maxleeds <30944219+maxleeds@users.noreply.github.com>
2017-08-14 21:23:11 -0400

    Update hive_schema_on_read.sql

--- max/hive_schema_on_read.sql
+++ max/hive_schema_on_read.sql
@@ -44,3 +44,32 @@ WITH SERDEPROPERTIES (
 STORED AS TEXTFILE
 LOCATION '/user/w205/mtastatic/complexes'
 ;
+
+DROP TABLE historicturnstile;
+
+CREATE EXTERNAL TABLE historicturnstile
+(
+	c_a string,
+	unit string,
+	scp string,
+	station string,
+	linename string,
+	divisiom string,
+	date string,
+	time string,
+	description string,
+	entries string,
+	exits string
+	
+)
+
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+ "separatorChar" = ',',
+ "quoteChar" = '"',
+
+ "escapeChar" = '\\'
+)
+STORED AS TEXTFILE
+LOCATION '/user/w205/mtastatic/turnstile'
+;

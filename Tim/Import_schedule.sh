36e7576120312ad38b08378f6a678324e33c22ad
TXu8 <timxu1@gmail.com>
2017-08-16 21:18:32 -0400

    Update Import_schedule.sh

--- Tim/Import_schedule.sh
+++ Tim/Import_schedule.sh
@@ -6,11 +6,9 @@ cat stops.txt > stops.csv
 tail -n +2 "stop_times.csv" > "stop_times1.csv"
 tail -n +2 "stops.csv" > "stops1.csv"
 
-hdfs dfs -rm /user/w205/final_project_test/stop_times/stop_times1.txt
-hdfs dfs -rm /user/w205/final_project_test/stops/stops1.txt
+hdfs dfs -mkdir /user/w205/staging
+hdfs dfs -mkdir /user/w205/staging/stop_times
+hdfs dfs -mkdir /user/w205/staging/stops
 
-hdfs dfs -mkdir /user/w205/final_project_test/stop_times
-hdfs dfs -mkdir /user/w205/final_project_test/stops
-
-hdfs dfs -put stop_times1.csv /user/w205/final_project_test/stop_times
-hdfs dfs -put stops1.csv /user/w205/final_project_test/stops
+hdfs dfs -put stop_times1.csv /user/w205/staging/stop_times
+hdfs dfs -put stops1.csv /user/w205/staging/stops

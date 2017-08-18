7485f2d44f5996877d6809aab05b2d6839e28591
maxleeds <30944219+maxleeds@users.noreply.github.com>
2017-08-17 20:07:47 -0400

    Update permissions.sh

--- max/permissions.sh
+++ max/permissions.sh
@@ -1,3 +1,8 @@
 chmod u+x,g+x staticimport.sh
 chmod u+x,g+x hive_schema_on_read.sql
 chmod u+x,g+x get_historic_turnstile.sh
+
+cd turnstileBatchUpdate
+chmod u+x,g+x new_turnstile_week.sh
+
+cd ..

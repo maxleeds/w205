09c6104a0794be5f516b3cd053c2113fc68a71c3
TXu8 <timxu1@gmail.com>
2017-08-20 21:10:47 -0400

    Update combine_stop_schedule.sql

--- Tim/combine_stop_schedule.sql
+++ Tim/combine_stop_schedule.sql
@@ -4,9 +4,16 @@ DROP TABLE combinedstopsdata;
 
 CREATE TABLE combinedstopsdata AS
 SELECT stopstimesdata.stop_id,
-trip_id,
-cast(concat("2016-01-01 ",arrival_time) as timestamp) as arrival_time,
-cast(concat("2016-01-01 ",departure_time) as timestamp) as departure_time,
+SUBSTR(trip_id, 1, INSTR(trip_id,"_")-4) schedule_day,
+SUBSTR(trip_id, INSTR(trip_id,"_")-3, 3) week_day,
+SUBSTR(trip_id, INSTR(trip_id,"_")+1, LENGTH(trip_id)) trip_id,
+cast(concat(cast(CURRENT_date as string)," ",arrival_time) as timestamp) as arrival_time,
+cast(concat(cast(CURRENT_date as string)," ",departure_time) as timestamp) as departure_time,
+unix_timestamp(concat(cast(CURRENT_date as string)," ",arrival_time)) as arrival_time_unix,
+unix_timestamp(concat(cast(CURRENT_date as string)," ",departure_time)) as departure_time_unix,
+CASE WHEN SUBSTR(trip_id, INSTR(trip_id,"_")-3, 3) = "SUN" THEN 7
+WHEN SUBSTR(trip_id, INSTR(trip_id,"_")-3, 3) = "WKD" THEN 1
+WHEN SUBSTR(trip_id, INSTR(trip_id,"_")-3, 3) = "SAT" THEN 6 END as week_day_num,
 stop_sequence,
 stop_name,
 cast(stop_lat as decimal(8,6)) as stop_latitude,

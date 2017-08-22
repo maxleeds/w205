b9f0ed0e22130e471b6e41d571810b5df9598d65
TXu8 <timxu1@gmail.com>
2017-08-21 23:49:56 -0400

    Update gtfsrt_updates.sh

--- Parse/gtfsrt_updates.sh
+++ Parse/gtfsrt_updates.sh
@@ -14,6 +14,7 @@ do
   hdfs dfs -put ./trip_update.csv /user/w205/mta/trip_update
   hdfs dfs -put ./vehicle_position.csv /user/w205/mta/vehicle_position
   hive -f ./combine_real_time_data.sql
+  mv /data/tim/w205/Parse/Output/000000_0 /data/tim/w205/Parse/Output/update.csv
   echo "sleeping for 30s"
   sleep 30s
   let "count=count+1"

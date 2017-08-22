c13a79eefcba4321af913088a119b5cd5501dae9
maxleeds <30944219+maxleeds@users.noreply.github.com>
2017-08-21 20:28:31 -0400

    Update new_turnstile_week.sh

--- max/turnstileBatchUpdate/new_turnstile_week.sh
+++ max/turnstileBatchUpdate/new_turnstile_week.sh
@@ -1,6 +1,6 @@
 
-sudo -u hdfs dfs -rm -r /user/w205/mtastatic/newturnstile
-sudo -u hdfs dfs -mkdir /user/w205/mtastatic/newturnstile
+sudo -u hdfs hdfs dfs -rm -r /user/w205/mtastatic/newturnstile
+sudo -u hdfs hdfs dfs -mkdir /user/w205/mtastatic/newturnstile
 
 x=$1
 url="http://web.mta.info/developers/data/nyct/turnstile/turnstile_$x.txt"
@@ -8,4 +8,4 @@ wget $url -O $x
 tail -n +2 $x > $x.final
 rm $x
 
-sudo -u hdfs dfs -put $x.final /user/w205/mtastatic/newturnstile
+sudo -u hdfs hdfs dfs -put $x.final /user/w205/mtastatic/newturnstile

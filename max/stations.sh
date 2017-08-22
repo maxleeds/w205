b91e514a3aa2d448f04e611746b38ffc4c3e1966
maxleeds <30944219+maxleeds@users.noreply.github.com>
2017-08-21 20:21:06 -0400

    Update stations.sh

--- max/stations.sh
+++ max/stations.sh
@@ -2,7 +2,7 @@
 if [ ! -d "$staging1" ]; then
   mkdir staging1
 fi
-hdfs dfs -mkdir /user/w205/mtastatic
+sudo -u hdfs dfs -mkdir /user/w205/mtastatic
 # import static list of stations
 
 cd staging1
@@ -10,12 +10,12 @@ url="http://web.mta.info/developers/data/nyct/subway/Stations.csv"
 wget $url -O stations.csv
 tail -n +2 stations.csv > stationsfinal.csv
 
-hdfs dfs -mkdir /user/w205/mtastatic/stations
-hdfs dfs -put stationsfinal.csv /user/w205/mtastatic/stations
+sudo -u hdfs dfs -mkdir /user/w205/mtastatic/stations
+sudo -u hdfs dfs -put stationsfinal.csv /user/w205/mtastatic/stations
 
 # import static list of complexes
 url1="http://web.mta.info/developers/data/nyct/subway/StationComplexes.csv"
 wget $url1 -O complexes.csv
 tail -n +2 complexes.csv > complexesfinal.csv
-hdfs dfs -mkdir /user/w205/mtastatic/complexes
-hdfs dfs -put complexesfinal.csv /user/w205/mtastatic/complexes
+sudo -u hdfs dfs -mkdir /user/w205/mtastatic/complexes
+sudo -u hdfs dfs -put complexesfinal.csv /user/w205/mtastatic/complexes

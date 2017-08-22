ffadcbded75ab65c797a50e01b38478bd8a7bf1f
TXu8 <timxu1@gmail.com>
2017-08-21 23:34:00 -0400

    Update Import_schedule.sh

--- Parse/Import_schedule.sh
+++ Parse/Import_schedule.sh
@@ -9,9 +9,9 @@ cat stops.txt > stops.csv
 tail -n +2 "stop_times.csv" > "stop_times1.csv"
 tail -n +2 "stops.csv" > "stops1.csv"
 
-sudo -u hdfs dfs -mkdir /user/w205/staging
-sudo -u hdfs dfs -mkdir /user/w205/staging/stop_times
-sudo -u hdfs dfs -mkdir /user/w205/staging/stops
+sudo -u hdfs hdfs dfs -mkdir /user/w205/staging
+sudo -u hdfs hdfs dfs -mkdir /user/w205/staging/stop_times
+sudo -u hdfs hdfs dfs -mkdir /user/w205/staging/stops
 
-sudo -u hdfs dfs -put stop_times1.csv /user/w205/staging/stop_times
-sudo -u hdfs dfs -put stops1.csv /user/w205/staging/stops
+sudo -u hdfs hdfs dfs -put stop_times1.csv /user/w205/staging/stop_times
+sudo -u hdfs hdfs dfs -put stops1.csv /user/w205/staging/stops

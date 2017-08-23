cbf091e73aaa74e23fc710e491615a61281923e0
TXu8 <timxu1@gmail.com>
2017-08-22 21:05:41 -0400

    Update gtfsrt_updates.sh

--- Parse/gtfsrt_updates.sh
+++ Parse/gtfsrt_updates.sh
@@ -6,13 +6,13 @@ while :
 do
   ./parse_gtfsrt.py
   echo "update number: $count"
-  hdfs dfs -mkdir /user/w205/mta 2> /dev/null
-  hdfs dfs -mkdir /user/w205/mta/trip_update 2> /dev/null
-  hdfs dfs -mkdir /user/w205/mta/vehicle_position 2> /dev/null
-  hdfs dfs -rm /user/w205/mta/trip_update/trip_update.csv &> /dev/null
-  hdfs dfs -rm /user/w205/mta/vehicle_position/vehicle_position.csv &> /dev/null
-  hdfs dfs -put ./trip_update.csv /user/w205/mta/trip_update
-  hdfs dfs -put ./vehicle_position.csv /user/w205/mta/vehicle_position
+  sudo -u hdfs hdfs dfs -mkdir /user/w205/mta 2> /dev/null
+  sudo -u hdfs hdfs dfs -mkdir /user/w205/mta/trip_update 2> /dev/null
+  sudo -u hdfs hdfs dfs -mkdir /user/w205/mta/vehicle_position 2> /dev/null
+  sudo -u hdfs hdfs dfs -rm /user/w205/mta/trip_update/trip_update.csv &> /dev/null
+  sudo -u hdfs hdfs dfs -rm /user/w205/mta/vehicle_position/vehicle_position.csv &> /dev/null
+  sudo -u hdfs hdfs dfs -put ./trip_update.csv /user/w205/mta/trip_update
+  sudo -u hdfs hdfs dfs -put ./vehicle_position.csv /user/w205/mta/vehicle_position
   hive -f ./combine_real_time_data.sql
   mv /data/tim/w205/Parse/Output/000000_0 /data/tim/w205/Parse/Output/update.csv
   echo "sleeping for 30s"

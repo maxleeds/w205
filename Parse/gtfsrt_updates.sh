47e1b273d1475e5b0b96320ca56f53871914e516
TXu8 <timxu1@gmail.com>
2017-08-22 23:10:19 -0400

    Update gtfsrt_updates.sh

--- Parse/gtfsrt_updates.sh
+++ Parse/gtfsrt_updates.sh
@@ -14,6 +14,7 @@ do
   sudo -u hdfs hdfs dfs -put ./trip_update.csv /user/w205/mta/trip_update
   sudo -u hdfs hdfs dfs -put ./vehicle_position.csv /user/w205/mta/vehicle_position
   hive -f ./combine_real_time_data.sql
+  # Update the paths here to the folder where you are storing your intermediate data
   mv /data/tim/w205/Parse/Output/000000_0 /data/tim/w205/Parse/Output/update.csv
   echo "sleeping for 30s"
   sleep 30s
